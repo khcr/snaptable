6a5508928e297363b9db7380ba0170a90cf369d7
khcr <kocher.ke@gmail.com>
2015-04-06 23:37:43 +0200

    assets

--- lib/autotable/railtie.rb
+++ lib/autotable/railtie.rb
@@ -1,9 +1,9 @@
-require 'autotable/table_constructor/table'
+# require 'autotable/table_constructor/table'
 
 module Autotable
   class Railtie < Rails::Railtie
-    initializer 'autotable.tables' do
-      ActionController::Base.send :include, TableConstructor
-    end
+    # initializer 'autotable.tables' do
+    #   ActionController::Base.send :include, TableConstructor
+    # end
   end
 end
