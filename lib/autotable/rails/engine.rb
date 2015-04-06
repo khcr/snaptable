237ad232ab635022fac1e576693007bb89ad54c8
khcr <kocher.ke@gmail.com>
2015-04-06 17:46:09 +0200

    precompile

--- lib/autotable/rails/engine.rb
+++ lib/autotable/rails/engine.rb
@@ -1,6 +1,9 @@
 module Autotable
   module Rails
     class Engine < ::Rails::Engine
+      initializer "Autotable.assets.precompile" do |app|
+        app.config.assets.precompile +=%w(autotable.js autotable.css)
+      end
     end
   end
 end
