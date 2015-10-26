8100d5cce6a29e29cd948707d84a53accbe2530e
khcr <kocher.ke@gmail.com>
2015-10-26 16:26:16 +0100

    require jquery

--- lib/snaptable/engine.rb
+++ lib/snaptable/engine.rb
@@ -3,6 +3,7 @@ require 'snaptable/helpers/table'
 module Snaptable
   class Engine < ::Rails::Engine
     require 'will_paginate'
+    require 'jquery-rails'
 
     initializer 'Snaptable.tables' do
       ActionController::Base.send :include, Helpers
--- lib/snaptable/version.rb
+++ lib/snaptable/version.rb
@@ -1,3 +1,3 @@
 module Snaptable
-  VERSION = "0.8.1"
+  VERSION = "0.8.3"
 end
